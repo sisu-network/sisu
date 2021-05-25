9a79d753c80026976e6bcfc6b1cd60ffb6feb9a7
Billy Pham <visopsys@users.noreply.github.com>
2021-05-25 12:10:24 -0700

    Add proto gen for tss

--- src/scripts/proto_gen.sh
+++ src/scripts/proto_gen.sh
@@ -3,12 +3,16 @@
 CUR_DIR=$(pwd)
 COSMOS_VERSION=0.42.1
 
-for f in $CUR_DIR/proto/evm/*
+for folder in $CUR_DIR/proto/**
 do
-  echo "Processing $f"
-protoc -I $CUR_DIR/proto/evm  \
-  -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/proto \
-  -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/third_party/proto \
-    --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:./x/evm/types \
-    $f
+  dir=$(basename $folder)
+  for file in $folder/*
+  do
+    protoc -I $CUR_DIR/proto/$dir  \
+      -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/proto \
+      -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/third_party/proto \
+        --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:./x/$dir/types \
+        $file
+  done
 done
+
