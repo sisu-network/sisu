edd07098dfa350a4a2fea97224e6fb80449c35ab
Benja Heart <visopsys@users.noreply.github.com>
2021-10-08 14:19:21 -0700

    - Correct API name

--- x/tss/api_handler.go
+++ x/tss/api_handler.go
@@ -1,9 +1,9 @@
 package tss
 
 import (
-	ethTypes "github.com/ethereum/go-ethereum/core/types"
-	eTypes "github.com/sisu-network/deyes/types"
-	dhTypes "github.com/sisu-network/dheart/types"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	etypes "github.com/sisu-network/deyes/types"
+	htypes "github.com/sisu-network/dheart/types"
 	"github.com/sisu-network/sisu/utils"
 	"github.com/sisu-network/sisu/x/tss/keeper"
 )
@@ -27,7 +27,7 @@ func (a *ApiHandler) Version() string {
 func (api *ApiHandler) CheckHealth() {
 }
 
-func (a *ApiHandler) KeygenResult(result dhTypes.KeygenResult) bool {
+func (a *ApiHandler) KeygenResult(result htypes.KeygenResult) bool {
 	utils.LogInfo("There is a Keygen Result")
 
 	a.processor.OnKeygenResult(result)
@@ -35,11 +35,11 @@ func (a *ApiHandler) KeygenResult(result dhTypes.KeygenResult) bool {
 }
 
 // This is a API endpoint to receive transactions with To address we are interested in.
-func (a *ApiHandler) PostObservedTxs(txs *eTypes.Txs) {
+func (a *ApiHandler) PostObservedTxs(txs *etypes.Txs) {
 	utils.LogDebug("There is new list of transactions from deyes")
 
 	for _, tx := range txs.Arr {
-		ethTx := &ethTypes.Transaction{}
+		ethTx := &ethtypes.Transaction{}
 
 		err := ethTx.UnmarshalBinary(tx.Serialized)
 		if err != nil {
@@ -51,7 +51,7 @@ func (a *ApiHandler) PostObservedTxs(txs *eTypes.Txs) {
 	a.processor.OnObservedTxs(txs)
 }
 
-func (a *ApiHandler) KeySignResult(result *dhTypes.KeysignResult) {
+func (a *ApiHandler) KeysignResult(result *htypes.KeysignResult) {
 	utils.LogInfo("There is keysign result")
 	go a.processor.OnKeysignResult(result)
 }
