e5a5a55d99b2e55a1451365d27b7a696620f4caa
Benja Heart <visopsys@users.noreply.github.com>
2021-10-21 14:19:40 -0700

    Check duplication for observed tx.

--- x/tss/types/constants.go
+++ x/tss/types/constants.go
@@ -3,6 +3,6 @@ package types
 const (
 	MSG_TYPE_KEYGEN_PROPOSAL = "KeygenProposal"
 	MSG_TYPE_KEYGEN_RESULT   = "KeygenResult"
-	MSG_TYPE_OBSERVED_TXS    = "ObservedTxs"
+	MSG_TYPE_OBSERVED_TX     = "ObservedTx"
 	MSG_TYPE_TX_OUT          = "TxOut"
 )
