5068c57119652ad46b58a87743a11fd024fa9408
Benja Heart <visopsys@users.noreply.github.com>
2021-11-09 11:26:15 -0800

    Add temp dir when generating testnet

--- cmd/sisud/cmd/gen/testnet.go
+++ cmd/sisud/cmd/gen/testnet.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/BurntSushi/toml"
@@ -27,14 +28,9 @@ type TestnetNode struct {
 	EyesIp  string `json:"eyes_ip"`
 }
 
-// type TestnetNodes struct {
-// 	Nodes []TestnetNode `json:"nodes"`
-// }
-
 const (
-	flagNodeFile = "node-file"
-	flagTomlFile = "toml-file"
-	flagChainId  = "chain-id"
+	flagTmpDir  = "tmp-dir"
+	flagChainId = "chain-id"
 )
 
 // get cmd to initialize all files for tendermint localnet and application
@@ -46,7 +42,7 @@ func TestnetCmd(mbm module.BasicManager, genBalIterator banktypes.GenesisBalance
 		Long: `privatenet creates configuration for a network with N validators.
 Example:
 	For multiple nodes (running with docker):
-	  ./sisu testnet --v 2 --output-dir ./output --chain-id testnet --node-file tmp/ips.json --toml-file tmp/sisu.toml
+	  ./sisu testnet --v 2 --output-dir ./output --chain-id testnet --tmp-dir tmp
 	`,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
@@ -56,15 +52,15 @@ Example:
 
 			serverCtx := server.GetServerContextFromCmd(cmd)
 			tmConfig := serverCtx.Config
+			tmConfig.LogLevel = "info"
 
 			outputDir, _ := cmd.Flags().GetString(flagOutputDir)
 			minGasPrices, _ := cmd.Flags().GetString(server.FlagMinGasPrices)
 			nodeDirPrefix, _ := cmd.Flags().GetString(flagNodeDirPrefix)
 			nodeDaemonHome, _ := cmd.Flags().GetString(flagNodeDaemonHome)
-			nodeFile, _ := cmd.Flags().GetString(flagNodeFile)
+			tempDir, _ := cmd.Flags().GetString(flagTmpDir)
 			chainId, _ := cmd.Flags().GetString(flagChainId)
 			numValidators, _ := cmd.Flags().GetInt(flagNumValidators)
-			flagTomlFile, _ := cmd.Flags().GetString(flagTomlFile)
 			algo, _ := cmd.Flags().GetString(flags.FlagKeyAlgorithm)
 
 			err = os.MkdirAll(outputDir, os.ModePerm)
@@ -83,15 +79,15 @@ Example:
 				monikers[i] = "node-talon-" + strconv.Itoa(i)
 			}
 
-			node := readNodeFile(nodeFile)
 			ips := make([]string, numValidators)
+			nodes := readTestnetNodes(tempDir, numValidators)
 
 			for i := 0; i < numValidators; i++ {
-				ips[i] = node.SisuIp
+				ips[i] = nodes[i].SisuIp
 			}
 			utils.LogInfo("ips = ", ips)
 
-			nodeConfigs := getTestnetNodeSettings(numValidators, flagTomlFile)
+			nodeConfigs := getTestnetNodeSettings(tempDir, numValidators)
 
 			settings := &Setting{
 				clientCtx:      clientCtx,
@@ -121,8 +117,7 @@ Example:
 	cmd.Flags().StringP(flagOutputDir, "o", "./output", "Directory to store initialization data for the localnet")
 	cmd.Flags().String(flagNodeDirPrefix, "node", "Prefix the directory name for each node with (node results in node0, node1, ...)")
 	cmd.Flags().String(flagNodeDaemonHome, "main", "Home directory of the node's daemon configuration")
-	cmd.Flags().String(flagNodeFile, "tmp/ips.json", "Location of ips.json file")
-	cmd.Flags().String(flagTomlFile, "tmp/sisu.toml", "Location of sisu.toml file")
+	cmd.Flags().String(flagTmpDir, "tmp-dir", "Location of temporary directory that contains list of peers ips and other configs.")
 	cmd.Flags().String(flagChainId, "talon-01", "Name of the chain")
 	cmd.Flags().String(server.FlagMinGasPrices, fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom), "Minimum gas prices to accept for transactions; All fees in a tx must meet this minimum (e.g. 0.01photino,0.001stake)")
 	cmd.Flags().String(flags.FlagKeyAlgorithm, string(hd.Secp256k1Type), "Key signing algorithm to generate keys for")
@@ -130,27 +125,35 @@ Example:
 	return cmd
 }
 
-func readNodeFile(path string) *TestnetNode {
-	content, err := os.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
+func readTestnetNodes(root string, numValidators int) []*TestnetNode {
+	nodeConfigs := make([]*TestnetNode, numValidators)
 
-	nodeConfigs := new(TestnetNode)
-	err = json.Unmarshal([]byte(content), nodeConfigs)
-	if err != nil {
-		panic(err)
+	for i := 0; i < numValidators; i++ {
+		path := filepath.Join(root, fmt.Sprintf("node%d", i), "ips.json")
+		content, err := os.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+
+		nodeConfig := new(TestnetNode)
+		err = json.Unmarshal([]byte(content), nodeConfig)
+		if err != nil {
+			panic(err)
+		}
+
+		nodeConfigs[i] = nodeConfig
 	}
 
 	return nodeConfigs
 }
 
-func getTestnetNodeSettings(numValidators int, flagTomlFile string) []config.Config {
+func getTestnetNodeSettings(root string, numValidators int) []config.Config {
 	nodeConfigs := make([]config.Config, numValidators)
 
 	for i := 0; i < numValidators; i++ {
 		cfg := config.Config{}
-		_, err := toml.DecodeFile(flagTomlFile, &cfg)
+		path := filepath.Join(root, fmt.Sprintf("node%d", i), "sisu.toml")
+		_, err := toml.DecodeFile(path, &cfg)
 		if err != nil {
 			panic(err)
 		}
