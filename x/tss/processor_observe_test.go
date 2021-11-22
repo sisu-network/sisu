68ca5bfe453e1a3238c16d52703185554c543ac5
Son Le <sonltcoder@gmail.com>
2021-11-22 11:18:03 +0700

    Observe test: success case

--- x/tss/processor_observe_test.go
+++ x/tss/processor_observe_test.go
@@ -7,20 +7,41 @@ import (
 	eyesTypes "github.com/sisu-network/deyes/types"
 	"github.com/sisu-network/sisu/config"
 	"github.com/sisu-network/sisu/tests/mock"
+	"github.com/sisu-network/sisu/utils"
 )
 
 func TestProcessor_OnObservedTxs(t *testing.T) {
 	t.Parallel()
 
-	processor := initProcessor()
 	t.Run("empty_txs", func(t *testing.T) {
 		t.Parallel()
 
+		processor := initProcessor()
 		processor.OnObservedTxs(&eyesTypes.Txs{})
 	})
 
 	t.Run("success_new_observe_txs", func(t *testing.T) {
 		t.Parallel()
+
+		mockDb := &mock.Database{}
+		mockDb.IsChainKeyAddressFunc = func(chain, address string) bool {
+			return true
+		}
+
+		txs := &eyesTypes.Txs{
+			Chain: utils.RandomString(10, utils.AlphaNumericCharacters),
+			Block: int64(utils.RandomNaturalNumber(1000)),
+			Arr: []*eyesTypes.Tx{{
+				Hash:       utils.RandomHeximalString(64),
+				Serialized: nil,
+				To:         utils.RandomHeximalString(64),
+				From:       utils.RandomHeximalString(64),
+			}},
+		}
+		processor := initProcessor()
+		processor.db = mockDb
+
+		processor.OnObservedTxs(txs)
 	})
 }
 
