ef6793d33eb72ccb1384334c05f18ede1f92962f
Son Le <sonltcoder@gmail.com>
2021-11-23 09:24:15 +0700

    Github flow

--- utils/strings.go
+++ utils/strings.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"math/rand"
+	"strings"
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
@@ -12,5 +13,15 @@ func GenerateRandomString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+// AllInAlphabet checks if all characters of s are in alphabet
+func AllInAlphabet(s string, alphabet string) bool {
+	for _, r := range s {
+		if !strings.ContainsRune(alphabet, r) {
+			return false
+		}
+	}
+	return true
 }
 
