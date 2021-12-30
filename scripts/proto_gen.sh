94cd423b63f35188df9fc7270817bd662a97560f
Benja Heart <visopsys@users.noreply.github.com>
2021-12-29 23:56:31 -0800

    Fix proto generation

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 CUR_DIR=$(pwd)
-COSMOS_VERSION=0.42.1
+COSMOS_VERSION=0.42.1-fork005
 
 for folder in $CUR_DIR/proto/**
 do
   dir=$(basename $folder)
   for file in $folder/*
   do
-    protoc -I $CUR_DIR/proto/$dir  \
-      -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/proto \
-      -I $GOPATH/pkg/mod/github.com/cosmos/cosmos-sdk@v$COSMOS_VERSION/third_party/proto \
+    protoc  \
+      -I $CUR_DIR \
+      -I $GOPATH/pkg/mod/github.com/sisu-network/cosmos-sdk@v$COSMOS_VERSION/proto \
+      -I $GOPATH/pkg/mod/github.com/sisu-network/cosmos-sdk@v$COSMOS_VERSION/third_party/proto \
         --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:./x/$dir/types \
         $file
+
+
+    cd ./x/$dir/types
+    mv proto/$dir/*.* .
+    rm -rf proto
+    cd ../../..
   done
 done
-
