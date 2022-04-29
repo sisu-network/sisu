b62b19ed2d0056f91d275182da4a4d5d56d994b2
Benja Heart <visopsys@users.noreply.github.com>
2022-04-28 17:15:14 -0700

    Fix polygon

--- cmd/sisud/cmd/dev/fund-sisu.go
+++ cmd/sisud/cmd/dev/fund-sisu.go
@@ -239,9 +239,9 @@ func (c *fundAccountCmd) transferEth(client *ethclient.Client, mnemonic, recipie
 		panic(err)
 	}
 
-	// 0.2 ETH
+	// 0.1 ETH
 	value := new(big.Int).Mul(utils.EthToWei, big.NewInt(int64(amount)))
-	value = new(big.Int).Div(value, big.NewInt(5))
+	value = new(big.Int).Div(value, big.NewInt(10))
 	gasLimit := uint64(21000) // in units
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
