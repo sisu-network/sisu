0120fe2d2fead23e8d746c3e765287c5c8a36b44
frog <95737177+frogchocolatebarpie@users.noreply.github.com>
2022-06-03 16:06:36 +0700

    Add tx out producer pause resume contract test

---
+++ x/sisu/tx_out_producer_pause_resume_contract_test.go
@@ -0,0 +1,97 @@
+package sisu
+
+import (
+	"testing"
+
+	"github.com/sisu-network/sisu/common"
+	"github.com/sisu-network/sisu/x/sisu/tssclients"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTxOutPauseResumeContract(t *testing.T) {
+	t.Parallel()
+
+	t.Run("can_pause_or_resume", func(t *testing.T) {
+		ctx := testContext()
+		keeper := keeperTestAfterContractDeployed(ctx)
+		deyesClient := &tssclients.MockDeyesClient{}
+		worldState := defaultWorldStateTest(ctx, keeper, deyesClient)
+		appKeys := common.NewMockAppKeys()
+		txOutputProducer := DefaultTxOutputProducer{
+			worldState: worldState,
+			keeper:     keeper,
+			appKeys:    appKeys,
+		}
+
+		chain := "ganache1"
+		hash := SupportedContracts[ContractErc20Gateway].AbiHash
+		txOutWithSigner, err := txOutputProducer.PauseOrResumeEthContract(ctx, chain, hash, false)
+		require.NoError(t, err)
+		require.NotNil(t, txOutWithSigner)
+
+		txOutWithSigner, err = txOutputProducer.PauseOrResumeEthContract(ctx, chain, hash, true)
+		require.NoError(t, err)
+		require.NotNil(t, txOutWithSigner)
+	})
+
+	t.Run("unsupported_chain", func(t *testing.T) {
+		ctx := testContext()
+		keeper := keeperTestAfterContractDeployed(ctx)
+		deyesClient := &tssclients.MockDeyesClient{}
+		worldState := defaultWorldStateTest(ctx, keeper, deyesClient)
+		appKeys := common.NewMockAppKeys()
+		txOutputProducer := DefaultTxOutputProducer{
+			worldState: worldState,
+			keeper:     keeper,
+			appKeys:    appKeys,
+		}
+
+		chain := "chain"
+		hash := SupportedContracts[ContractErc20Gateway].AbiHash
+		txOutWithSigner, err := txOutputProducer.PauseOrResumeEthContract(ctx, chain, hash, false)
+		require.Error(t, err)
+		require.Nil(t, txOutWithSigner)
+	})
+
+	t.Run("can_not_find_gateway_address", func(t *testing.T) {
+		ctx := testContext()
+		keeper := keeperTestAfterKeygen(ctx)
+		deyesClient := &tssclients.MockDeyesClient{}
+		worldState := defaultWorldStateTest(ctx, keeper, deyesClient)
+		appKeys := common.NewMockAppKeys()
+		txOutputProducer := DefaultTxOutputProducer{
+			worldState: worldState,
+			keeper:     keeper,
+			appKeys:    appKeys,
+		}
+
+		chain := "ganache1"
+		hash := SupportedContracts[ContractErc20Gateway].AbiHash
+		txOutWithSigner, err := txOutputProducer.PauseOrResumeEthContract(ctx, chain, hash, false)
+		require.Error(t, err)
+		require.Nil(t, txOutWithSigner)
+	})
+
+	t.Run("can_not_find_nonce", func(t *testing.T) {
+		ctx := testContext()
+		keeper := keeperTestAfterContractDeployed(ctx)
+		deyesClient := &tssclients.MockDeyesClient{}
+		deyesClient.GetNonceFunc = func(chain, address string) int64 {
+			return -1
+		}
+
+		worldState := defaultWorldStateTest(ctx, keeper, deyesClient)
+		appKeys := common.NewMockAppKeys()
+		txOutputProducer := DefaultTxOutputProducer{
+			worldState: worldState,
+			keeper:     keeper,
+			appKeys:    appKeys,
+		}
+
+		chain := "ganache1"
+		hash := SupportedContracts[ContractErc20Gateway].AbiHash
+		txOutWithSigner, err := txOutputProducer.PauseOrResumeEthContract(ctx, chain, hash, false)
+		require.Error(t, err)
+		require.Nil(t, txOutWithSigner)
+	})
+}
