2f9e8befb1a8c471b846368db2359739ebec87c5
Benja Heart <visopsys@users.noreply.github.com>
2022-07-18 11:10:04 +0700

    WIP

--- x/sisu/helper/helpers.go
+++ x/sisu/helper/helpers.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"math/big"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	ctypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/sisu-network/sisu/x/sisu/keeper"
 )
 
 func GetKeygenId(keyType string, block int64, pubKeys []ctypes.PubKey) string {
@@ -18,6 +21,17 @@ func GetKeygenId(keyType string, block int64, pubKeys []ctypes.PubKey) string {
 	hash := hex.EncodeToString(digester.Sum(nil))
 
 	return fmt.Sprintf("%s;%d;%s", keyType, block, hash)
+}
+
+func GetChainGasCostInToken(ctx sdk.Context, k keeper.Keeper, tokenId, chainId string) (*big.Int, error) {
+	chain := k.GetChain(ctx, chainId)
+	gasPrice := chain.GasPrice
+	token := k.GetTokens(ctx, []string{tokenId})[tokenId]
+	if token == nil {
+		return nil, fmt.Errorf("GetChainGasCostInToken: cannot find token ", tokenId)
+	}
+
+	price, ok := new(big.Int).SetString(token.Price, 10)
 }
 
 func GetGasCostInToken(gas, gasPrice, tokenPrice, nativeTokenPrice *big.Int) (*big.Int, error) {
