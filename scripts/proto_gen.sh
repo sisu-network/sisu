e9612a2046821f4f12b606c7ed244daf62e6c116
Benja Heart <visopsys@users.noreply.github.com>
2023-01-21 11:54:33 -0800

    Move gogoproto to proto folder

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -9,7 +9,6 @@ dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirna
 for dir in $dirs; do
   protoc \
     -I "proto" \
-    -I "external/proto" \
     --gocosmos_out=plugins=interfacetype+grpc,Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
     $(find "${dir}" -maxdepth 1 -name '*.proto')
 done
